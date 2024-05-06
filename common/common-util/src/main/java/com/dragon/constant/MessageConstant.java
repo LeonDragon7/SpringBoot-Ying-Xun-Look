16afeb03202d06d775635d93e843819aa93d51e5
LeonDragon7 <209418465@qq.com>
2024-05-06 22:38:56 +0800

    feat: 信息提示常量类

--- common/common-util/src/main/java/com/dragon/constant/MessageConstant.java
+++ common/common-util/src/main/java/com/dragon/constant/MessageConstant.java
@@ -16,7 +16,8 @@ public enum MessageConstant {
     UPLOAD_FAIL(201,"文件上传失败"),
     MERGE_FAIL(201,"文件合并失败"),
     DELETE_FAIL(201,"文件删除失败"),
-    FILE_NO(201,"文件不存在");
+    FILE_NO(201,"文件不存在"),
+    EMAIL_FORMAT_ERROR(201,"邮箱格式不正确");
 
 
     private Integer code;
