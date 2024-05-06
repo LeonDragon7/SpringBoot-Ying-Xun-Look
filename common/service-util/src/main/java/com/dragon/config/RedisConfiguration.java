796535f69039e4fa42319a59fbcf5d3a63c04d77
LeonDragon7 <209418465@qq.com>
2024-05-06 22:37:57 +0800

    feat: redis配置类

---
+++ common/service-util/src/main/java/com/dragon/config/RedisConfiguration.java
@@ -0,0 +1,22 @@
+package com.dragon.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate redisTemplate = new RedisTemplate();
+        //设置redis的连接工厂
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        //设置redis key的序列化器
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+
+}
