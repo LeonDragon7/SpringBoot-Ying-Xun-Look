2808c46e4ccfbcf7fba6ac230a707a38c0d0c494
LeonDragon7 <209418465@qq.com>
2024-05-28 20:58:23 +0800

    feat: 配置静态资源文件

--- common/spring-security/src/main/java/com/dragon/config/WebSecurityConfig.java
+++ common/spring-security/src/main/java/com/dragon/config/WebSecurityConfig.java
@@ -48,7 +48,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .cors().and()
                 .authorizeRequests()
                 // 指定某些接口不需要通过验证即可访问。登陆接口肯定是不需要认证的
-                .antMatchers("/client/user/login","/favicon.ico","/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**", "/doc.html","/client/user/**").permitAll()
+                .antMatchers("/client/user/login").permitAll()
                 // 这里意思是其它所有接口需要认证才能访问
                 .anyRequest().authenticated()
                 .and()
@@ -75,7 +75,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/favicon.ico","/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**",
-                "/doc.html","/client/user/**","/client/index/**","/client/video/{id}","/client/video/like","/client/video/pageQuery", "/static/**");
+        web.ignoring().antMatchers("/favicon.ico","/static/**","/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**",
+                "/doc.html","/client/user/**","/client/index/**","/client/video/{id}","/client/video/like","/client/video/pageQuery");
     }
 }
